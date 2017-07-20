9bc28bc38765d19ad453c4857236bce74e7823b9
Tran Huu Nguyen <nguyenth@dasannetworks.com>
2017-07-20 14:17:36 +0700

    Implement getMibNodeInformation

--- src/main/java/com/dasanzhone/seawind/swservice/snmp/SnmpUtil.java
+++ src/main/java/com/dasanzhone/seawind/swservice/snmp/SnmpUtil.java
@@ -1,5 +1,8 @@
 package com.dasanzhone.seawind.swservice.snmp;
 
+import com.adventnet.snmp.beans.SnmpTarget;
+import com.adventnet.snmp.mibs.MibNode;
+import com.adventnet.snmp.mibs.MibOperations;
 import com.adventnet.snmp.snmp2.SnmpAPI;
 import com.adventnet.snmp.snmp2.SnmpException;
 import com.adventnet.snmp.snmp2.SnmpOID;
@@ -8,6 +11,62 @@ import com.adventnet.snmp.snmp2.SnmpVar;
 import com.adventnet.snmp.snmp2.SnmpVarBind;
 
 public class SnmpUtil {
+
+    public static String getMibNodeInformation(String mibfile, String OID) {
+
+//        if( args.length < 2)
+//        {
+//            System.out.println("Usage : java MibNodeInfo mibfile OID ");
+//            System.exit(0);
+//        }
+
+        // Take care of getting OID and the MIB file name
+
+//        String mibfile = args[0];
+//        String OID = args[1];
+
+        SnmpTarget target=new SnmpTarget();
+        try{
+            target.loadMibs(mibfile);
+        }catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+
+        // load the MIB file
+        MibOperations mibops = target.getMibOperations();
+        try {
+            mibops.loadMibModules(mibfile);
+        } catch (Exception ex){
+            System.err.println("Error loading MIBs: "+ex);
+        }
+
+        // add OIDs
+
+        SnmpOID oid = mibops.getSnmpOID(OID);
+        MibNode node = mibops.getMibNode(oid);
+
+        if(node == null)
+        {
+            System.out.println("Invalid OID / the node " + oid + " is not available");
+        }
+        else
+        {
+            String result = "Syntax:"+node.getSyntax()+"\n"+
+                "Access:"+node.printAccess()+"\n"+
+                "Status:"+node.getStatus()+"\n"+
+                "Reference:"+node.getReference()+"\n"+
+                "OID:"+node.getNumberedOIDString()+"\n"+
+                "Node:"+node.getOIDString()+"\n"+
+                "Description:"+node.getDescription()+"\n";
+
+            System.out.println(result);
+
+            return result;
+        }
+
+        return null;
+    }
 
 	public static SnmpVar getSnmpVar(String type, String value) {
 		
